2fe30eef7c6355b181c4080187b124cdb5327e21
sksaifuddin <atushaik@gmail.com>
2018-08-29 12:32:54 +0530

    created basic structure of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { MaterialModule } from './shared/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
